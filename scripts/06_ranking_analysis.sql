8d80202cedb24ea59a20cbee8bcc205f30921cdf
Kgonlez <Kgonlez54@gmail.com>
2025-03-06 17:13:58 -0800

    Update 06_ranking_analysis.sql

--- scripts/06_ranking_analysis.sql
+++ scripts/06_ranking_analysis.sql
@@ -7,21 +7,22 @@ Purpose:
     - To identify top performers or laggards.
 
 SQL Functions Used:
-    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
+    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), LIMIT
     - Clauses: GROUP BY, ORDER BY
 ===============================================================================
 */
 
 -- Which 5 products are generating the Highest Revenue?
 -- Simple Ranking
-SELECT TOP 5
+SELECT 
     p.product_name,
     SUM(f.sales_amount) AS total_revenue
 FROM `gold.fact_sales` f
 LEFT JOIN `gold.dim_products` p
     ON p.product_key = f.product_key
 GROUP BY p.product_name
-ORDER BY total_revenue DESC;
+ORDER BY total_revenue DESC
+LIMIT 5;
 
 -- Complex but Flexibly Ranking Using Window Functions
 SELECT *
@@ -38,17 +39,18 @@ FROM (
 WHERE rank_products <= 5;
 
 -- What are the 5 worst-performing products in terms of sales?
-SELECT TOP 5
+SELECT 
     p.product_name,
     SUM(f.sales_amount) AS total_revenue
 FROM `gold.fact_sales` f
 LEFT JOIN `gold.dim_products` p
     ON p.product_key = f.product_key
 GROUP BY p.product_name
-ORDER BY total_revenue;
+ORDER BY total_revenue
+LIMIT 5;
 
 -- Find the top 10 customers who have generated the highest revenue
-SELECT TOP 10
+SELECT 
     c.customer_key,
     c.first_name,
     c.last_name,
@@ -60,10 +62,11 @@ GROUP BY
     c.customer_key,
     c.first_name,
     c.last_name
-ORDER BY total_revenue DESC;
+ORDER BY total_revenue DESC
+LIMIT 10;
 
 -- The 3 customers with the fewest orders placed
-SELECT TOP 3
+SELECT 
     c.customer_key,
     c.first_name,
     c.last_name,
@@ -75,4 +78,5 @@ GROUP BY
     c.customer_key,
     c.first_name,
     c.last_name
-ORDER BY total_orders ;
+ORDER BY total_orders 
+LIMIT 3;
